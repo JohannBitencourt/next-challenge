c80868f7373b34841c5d1900a884fc6d5a09d71c
JohannBitencourt <johann.bitencourt@gmail.com>
2020-12-22 15:44:37 +0000

    first

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure.</p>
+    </>
+  )
+}
+
+export default Home
